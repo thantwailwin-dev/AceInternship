1352cec6c654b2e9b85242c2d01ffd62e9ecf7d8
ThantWaiLwin <152428057+ThantWL@users.noreply.github.com>
2024-06-19 11:18:55 +0630

    Ado Dot Net CRUD [ ConsoleApp ]

---
+++ InternshipDotNetCore.ConsoleApp/AdoDotNetExamples/AdoDotNetExample.cs
@@ -0,0 +1,136 @@
+﻿using InternshipDotNetCore.ConsoleApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipDotNetCore.ConsoleApp.AdoDotNetExamples
+{
+    internal class AdoDotNetExample
+    {
+        public void Run()
+        {
+            Read();
+            /*Edit(25);*/
+            /*Create("ace","tanaka","jp");*/
+            /*Update(22,"ace", "tanaka", "jp");*/
+            /*Delete(18);*/
+        }
+
+        private void Create(string title,string content,string author)
+        {
+            SqlConnection connection = new SqlConnection(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+                                   ([BlogTitle]
+                                   ,[BlogAuthor]
+                                   ,[BlogContent])
+                             VALUES
+                                   (@BlogTitle
+                                   ,@BlogAuthor
+                                   ,@BlogContent)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);            
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+
+            string message = result > 0 ? "Saving Successful!" : "Saving Failed!";
+            Console.WriteLine(message);
+        }
+
+        private void Read()
+        {
+            SqlConnection connection = new SqlConnection(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+            string query = "select * from tbl_blog";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            connection.Close();
+
+            foreach(DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("Blog Id => " + dr["BlogId"]);
+                Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+                Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+                Console.WriteLine("Blog Content => " + dr["BlogContent"]);                
+            }
+
+        }        
+
+        private void Edit(int id)
+        {
+            SqlConnection connection = new SqlConnection(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+            string query = "select * from tbl_blog where BlogId=@BlogId";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@BlogId",id);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            connection.Close();
+
+            if (dt.Rows.Count == 0)
+            {
+                Console.WriteLine("No Data Found!");
+                return;
+            }
+
+            DataRow dr = dt.Rows[0];
+
+            Console.WriteLine("Blog Id => " + dr["BlogId"]);
+            Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+            Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+            Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+
+        }
+
+        private void Update(int id,string title,string author,string content)
+        {
+            SqlConnection connection = new SqlConnection(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+            string query = @"UPDATE [dbo].[Tbl_Blog]
+                           SET [BlogTitle] = @BlogTitle,
+                               [BlogAuthor] = @BlogAuthor,
+                              [BlogContent] = @BlogContent
+                         WHERE BlogId=@BlogId";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);            
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+
+            string message = result > 0 ? "Updating Successful!" : "Updating Failed!";
+            Console.WriteLine(message);
+        }
+
+        private void Delete(int id)
+        {
+            SqlConnection connection = new SqlConnection(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+            string query = "delete from tbl_blog where BlogId = @BlogId";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+
+            string message = result > 0 ? "Deleting Successful!" : "Deleting Failed!";
+            Console.WriteLine(message);
+        }
+    }
+}
--- InternshipDotNetCore.ConsoleApp/Program.cs
+++ InternshipDotNetCore.ConsoleApp/Program.cs
@@ -1,10 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 /*Console.WriteLine("Hello, World!");*/
 
+using InternshipDotNetCore.ConsoleApp.AdoDotNetExamples;
 using InternshipDotNetCore.ConsoleApp.EFCoreExamples;
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+adoDotNetExample.Run();
 
 /*DapperExample dapper = new DapperExample();
 dapper.Run();*/
 
-EFCoreExample eFCoreExample = new EFCoreExample();
-eFCoreExample.Run();
+/*EFCoreExample eFCoreExample = new EFCoreExample();
+eFCoreExample.Run();*/
